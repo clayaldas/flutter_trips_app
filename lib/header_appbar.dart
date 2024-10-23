518fdf776f1effbd4d9992542ce9239fb8038cdf
Clay Aldas <clayaldas@gmail.com>
2024-10-23 16:32:34 -0500

    Proyecto creado

---
+++ lib/header_appbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_trips_app/card_image_list.dart';
+import 'card_image_list.dart';
+// import 'package:flutter_trips_app/gradient_back.dart';
+import 'gradient_back.dart';
+
+class HeaderAppBar extends StatelessWidget {
+  const HeaderAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Stack(
+      children: [
+        GradientBack(title: 'Popular'),
+        CardImageList(),
+      ],
+    );
+  }
+}
---
+++ lib/profile_trips_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_trips_app/profile_background.dart';
+import 'profile_background.dart';
+// import 'package:flutter_trips_app/profile_header.dart';
+import 'profile_header.dart';
+// import 'package:flutter_trips_app/profile_places_list.dart';
+import 'profile_places_list.dart';
+
+class ProfileTripsPage extends StatelessWidget {
+  const ProfileTripsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const ProfileBackground(),
+        ListView(
+          children: [
+            const ProfileHeader(),
+            ProfilePlacesList(),
+          ],
+        ),
+      ],
+    );
+  }
+}
